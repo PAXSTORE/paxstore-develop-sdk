7cbc83a9d46f65fc6aebaee818d6f18f9feb65d7
毛正文 <546166705@qq.com>
2024-11-18 16:35:58 +0800

    revert TestConstants

--- src/test/java/com/pax/market/api/sdk/java/api/test/TestConstants.java
+++ src/test/java/com/pax/market/api/sdk/java/api/test/TestConstants.java
@@ -17,16 +17,9 @@ package com.pax.market.api.sdk.java.api.test;
  * @date  2018/11/02 15:23:22
  */
 public interface TestConstants {
-//  pax_developer_3rd_sys表数据
+
 	public static final String API_BASE_URL = "http://www.paxdev.com:8085/p-market-api";
-	public static final String API_KEY = "P3JZPHF8TZERYI9WXR4O";
-	public static final String API_SECRET = "D4S5LG6GOWLVD47SQEEMJ9H96MHHMT19XZLKGYIO";
-//	public static final String API_BASE_URL = "https://api.sit.whatspos.cn/p-market-api";
-//	public static final String API_KEY = "P3JZPHF8TZERYI9WXR4O";//"FA8AGLCJ51UHK7FVPEGU";
-//	public static final String API_SECRET = "D4S5LG6GOWLVD47SQEEMJ9H96MHHMT19XZLKGYIO";//"CWL2OU9QZB6PK1MB6H0YFU83MHLLCVO8D9XXOMI0";
-
-
-
+	public static final String API_KEY = "7AN2R0ROMLCOZI39H0MV";
+	public static final String API_SECRET = "VGS7NRNRYT66PUD8P4NEYB3HO0X7BHWPM15MIGW7";
 
 }
-
